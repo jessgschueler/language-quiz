c787e03506be755b6c6adffade8dbce0034cf53d
Jess Schueler <jessicagrace.schueler@gmail.com>
2022-03-25 11:33:14 -0700

    Change result display from page redirect to jQuery hide/show function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
 $(document).ready(function() {
-  if ("input:radio[id=veriperi]:checked") {
-    $(".veriperi").addclass()
-  } else;
-
-});
-
-
-$(document).ready(function() {
   $("form").submit (function(event) {
     event.preventDefault();
     const result1 = parseInt($("input:radio[name=q1]:checked").val());
@@ -15,15 +7,34 @@ $(document).ready(function() {
     const result4 = parseInt($("input:radio[name=q4]:checked").val());
     const result5 = parseInt($("input:radio[name=q5]:checked").val());
     const result = result1 + result2 + result3 + result4 + result5
-    
+
     if (result >= 36) {
-      window.location.href="swift.html"
-    } else if (result >= 15 && result <= 35) {
-      window.location.href="java.html"
-    } else if (result <= 14) {
-      window.location.href="python.html"
+      $("#swift").show()
     } else {
-      window.location.href="enigma.html"
+      $("#swift").hide()
     }
+
+    if (result >= 15 && result <= 35) {
+      $("#java").show()
+    } else {
+      $("#java").hide()
+    }
+    
+    if (result <= 14) {
+      $("#python").show()
+    } else {
+      $("#python").hide()
+    }
+    // if (result >= 36) {
+    //   window.location.href="swift.html"
+    // } else if (result >= 15 && result <= 35) {
+    //   window.location.href="java.html"
+    // } else if (result <= 14) {
+    //   window.location.href="python.html"
+    // } else {
+    //   window.location.href="enigma.html"
+    // }
+
+
   });
 });
