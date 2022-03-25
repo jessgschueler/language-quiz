6739db703afa9a236f133bd3365227e92b5dd60c
Jess Schueler <jessicagrace.schueler@gmail.com>
2022-03-25 14:59:25 -0700

    Add custom font.

--- js/scripts.js
+++ js/scripts.js
@@ -28,25 +28,3 @@ $(document).ready(function() {
 
   });
 });
-
-// $(document).ready(function() {
-//   $("form").submit (function(event) {
-//     event.preventDefault();
-//     const result1 = parseInt($("input:radio[name=q1]:checked").val());
-//     const result2 = parseInt($("input:radio[name=q2]:checked").val());
-//     const result3 = parseInt($("input:radio[name=q3]:checked").val());
-//     const result4 = parseInt($("input:radio[name=q4]:checked").val());
-//     const result5 = parseInt($("input:radio[name=q5]:checked").val());
-//     const result = result1 + result2 + result3 + result4 + result5
-
-//     if (result >= 36 && result !== 40) {
-//       $("form").addClass(".swiftpage")
-//     } else if (result >= 15 && result <= 35 && result !== 24 || result === 40) {
-//       $("form").addClass(".javapage")
-//      } else if (result <= 14 || result === 24) {
-//       $("form").addClass(".pythonpage")
-//     } else {
-//       alert("uh-oh! please fill out all options!")
-//     }
-//   });
-// });
